4e43b54a17f6c361e7c48818849744e120246ccf
DominiqueYoumans <ddyoumans@icloud.com>
2020-05-08 08:27:47 -0700

    add normalize method to move faster diagonally than it will along a single axis.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -15,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis ("Horizontal");
         float vertical = Input.GetAxis ("Vertical");
         m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
     
     }
 
