3595227f60269e41529834af14cd606e44e5c527
Dom <dom@Doms-iMac.local>
2020-05-21 06:45:56 -0700

    update the update method to show object has been hit.

--- Casting.cs
+++ Casting.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 using Sytem.Collections;
 
 public class Casting : MonoBehaviour {
-    
+    public float TargetDistance:
+
+    void Update() {
+        RaycastHit TheHit;
+
+        
+        
+    }
+
 }
