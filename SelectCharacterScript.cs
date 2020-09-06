ab854339f0ad0ce6cc42e95f8518171cc1729584
Dom Youmans <ddyoumans@icloud.com>
2020-09-06 15:22:17 -0700

    add game object and character index.

--- SelectCharacterScript.cs
+++ SelectCharacterScript.cs
@@ -2,3 +2,9 @@ using System.Collections;
 using.System.Collections.Generic;
 using UnityEngine;
 
+public class SelectCharacterScript : MonoBehaviour
+{
+    GameObject[] Characters;
+    int CharacterIndex;
+    
+}
